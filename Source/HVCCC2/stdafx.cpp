e548dd6ef73a5615cc7c14407783694ce3568045
Legend9119 <nicholaskhendry@gmail.com>
2018-07-12 21:40:42 +1000

    Added stdafx to repo.

---
+++ Source/HVCCC2/stdafx.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "stdafx.h"
+
+stdafx::stdafx()
+{
+}
+
+stdafx::~stdafx()
+{
+}
---
+++ Source/HVCCC2/stdafx.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+//#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+/**
+ * 
+ */
+class HVCCC2_API stdafx
+{
+public:
+	stdafx();
+	~stdafx();
+};
