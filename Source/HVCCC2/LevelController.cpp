6337366866ce0f18290a6c9ab288873a0d87bd9d
Sebastian Brown <developer2025@gmail.com>
2018-08-26 19:37:45 +1000

    Runs without crashing with tick function commented

--- Source/HVCCC2/LevelController.cpp
+++ Source/HVCCC2/LevelController.cpp
@@ -8,6 +8,14 @@
 #include "ConveyorBelt.h"
 #include <deque>
 
+#include "scheduling/data/serialization.h"
+
+#include "scheduling/data/env_config.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
+
+#include <algorithm>
+
+
 using namespace std;
 
 deque<ACoalStack*> coalStacks;
@@ -22,14 +30,20 @@ float mock_level;
 // Sets default values
 ALevelController::ALevelController()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	xMin = std::numeric_limits<double>::infinity();
+	xMax = -std::numeric_limits<double>::infinity();
+	worldTime = 0;
+	speed = 1;
 
 }
 
 // Called when the game starts or when spawned
 void ALevelController::BeginPlay()
 {
+	Super::BeginPlay();
+
 	// Level setup procedure
 
 	testTime = 0;
@@ -61,16 +75,97 @@ void ALevelController::BeginPlay()
 	coalStacks.at(1)->setQuantity(0.2);
 	coalStacks.at(2)->setQuantity(0.5);
 
+
+
+
+	//DATA STUFF
 	
 
-	Super::BeginPlay();
+	FVector startPoint(-425, -425, 100);
+	FVector endPoint(425, 425, 100);
+
+	bool success = deserialize(BINARY_PATH, states);
+	int i = 0;
+	for (auto eachObj : std::get<std::map<std::string, std::vector<StackerState>>>(states)) {
+		FString fstr = UTF8_TO_TCHAR(eachObj.first.c_str());
+		UE_LOG(LogTemp, Warning, TEXT("Stacker name: %s"), *fstr);
+		for (auto eachState : eachObj.second) {
+			UE_LOG(LogTemp, Warning, TEXT("index: %d, Time: %f; Stacker Position: %f"), i++, eachState.time, eachState.position);
+			if (eachState.position < xMin) {
+				xMin = eachState.position;
+			}
+			if (eachState.position > xMax) {
+				xMax = eachState.position;
+			}
+		}
+	}
+	//daCube = spawnAGlowCube(startPoint, endPoint);
+	//UE_LOG(LogTemp, Warning, TEXT("Pointer id: %s"), daCube);
+	auto firstObj = std::get<std::map<std::string, std::vector<StackerState>>>(states)["3.58"];
+	theWatch.first = 0;
+	theWatch.second = std::min(std::size_t(1), firstObj.size() - 1);
+
+
+
+
+
+
+
 	
 }
 
 // Called every frame
 void ALevelController::Tick(float DeltaTime)
 {
-	// TEST INPUT
+	Super::Tick(DeltaTime);
+
+	// DATA STUFF
+
+	/*
+	moveTime(DeltaTime * speed);
+	auto firstObjStates = std::get<std::map<std::string, std::vector<StackerState>>>(states)["3.58"];
+
+	int indexA = theWatch.first;
+	int indexB = theWatch.second;
+	*/
+	
+
+
+	/*while (firstObjStates[indexA].type == firstObjStates[indexB].type && firstObjStates[indexA].position == firstObjStates[indexB].position && theWatch.second < firstObjStates.size() - 1) {
+	++indexB;
+	}*/
+
+	/*
+	double timeA = firstObjStates[indexA].time;
+	double timeB = firstObjStates[indexB].time;
+
+	//the length of time available between the states
+	double aToBTimeDist = timeB - timeA;
+
+	//we have to limit the target time in case the worldTime is beyond the current frame
+	double targetTime = std::max(timeA, std::min(timeB, worldTime));
+
+	//determine the scale as a 
+	double scale = aToBTimeDist > 0 ? (targetTime - timeA) / aToBTimeDist : 0;
+
+
+	double positionA = firstObjStates[indexA].position;
+	double positionB = firstObjStates[indexB].position;
+
+	double positionInterpolated = positionA + (positionB - positionA)*(scale);
+
+	double positionDelta = (positionInterpolated - xMin) / (xMax - xMin);
+
+	UE_LOG(LogTemp, Warning, TEXT("Time: %f; state a: %d, state b: %d, typea: %d, typeb: %d"), float(worldTime), indexA, indexB, (int)firstObjStates[indexA].type, (int)firstObjStates[indexB].type);
+	UE_LOG(LogTemp, Warning, TEXT("scale: %f, timeA: %f, timeb: %f positiona: %f, positionb: %f, positionInterpolated: %f Position delta: %f"), float(scale), float(timeA), float(timeB), float(positionA), float(positionB), float(positionInterpolated), float(positionDelta));
+
+	//daCube->applyState(positionDelta);
+	*/
+	
+
+
+
+	/* TEST INPUT
 	stackerReclaimers.at(0)->setPosition(0.15);
 	stackerReclaimers.at(1)->setPosition(0.8);
 	stackerReclaimers.at(0)->setRotation(120);
@@ -103,10 +198,25 @@ void ALevelController::Tick(float DeltaTime)
 
 	}
 		coalStacks.at(0)->setQuantity(mock_level);
+		*/
 
-	Super::Tick(DeltaTime);
 
 }
+
+
+
+void ALevelController::moveTime(double deltaTime) {
+	worldTime += deltaTime;
+	auto firstObjStates = std::get<std::map<std::string, std::vector<StackerState>>>(states)["3.58"];
+	while (firstObjStates[theWatch.second].time < worldTime && theWatch.second < firstObjStates.size() - 1) {
+		++theWatch.first;
+		++theWatch.second;
+	}
+	if (firstObjStates[theWatch.second].time < worldTime) {
+		theWatch.first = theWatch.second;
+	}
+}
+
 
 
 /*
--- Source/HVCCC2/LevelController.h
+++ Source/HVCCC2/LevelController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "scheduling/data/serialization.h"
 
 
 #include "LevelController.generated.h"
@@ -13,6 +14,20 @@ UCLASS()
 class HVCCC2_API ALevelController : public AActor
 {
 	GENERATED_BODY()
+	
+	StateMapTuple states;
+	double xMin, xMax;
+
+	std::pair<std::size_t, std::size_t> theWatch;
+
+	double worldTime;
+	double speed;
+
+	//AGlowCube *daCube;
+
+	//AGlowCube * spawnAGlowCube(FVector start, FVector end);
+
+	void moveTime(double deltaTime);
 	
 public:	
 	// Sets default values for this actor's properties
