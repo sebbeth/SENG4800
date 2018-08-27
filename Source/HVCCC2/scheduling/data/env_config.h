413ebedcc431bea13076910ac64f90482a3b2569
sebbeth <develope2025@gmail.com>
2018-08-27 11:56:03 +1000

    This works on lab pc

--- Source/HVCCC2/scheduling/data/env_config.h
+++ Source/HVCCC2/scheduling/data/env_config.h
@@ -1,6 +1,6 @@
 #include <string>
 #include <cstdlib>
-#define PROJECT_PATH std::string("F:/seng4211/SENG4800/Source/HVCCC2/scheduling/data") 
+#define PROJECT_PATH std::string("C:/Users/SENG/Documents/SENG48002018/SENG4800/Source/HVCCC2/scheduling/data") 
 #define XML_PATH PROJECT_PATH+"/XMLs"
 #define BINARY_PATH PROJECT_PATH+"/BinaryDataFiles"
 //#define BINARY_PATH std::getenv("BINARY_PATH")
