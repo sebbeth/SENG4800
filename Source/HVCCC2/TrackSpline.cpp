6694df61f63e429712f03dd38838c2df0911bf29
Nath <c3187164@uon.edu.au>
2018-09-16 23:20:17 +1000

    added train movment
    Train movement works now, however there is still a lot to clean up code wise and at present cannot get code data structure of a floatcurve to function only the created one in the editor no idea why... and this is a issue as this means the animation cannot be updated by the data if the code data structure of a floatcurve wont work

--- Source/HVCCC2/TrackSpline.cpp
+++ Source/HVCCC2/TrackSpline.cpp
@@ -8,28 +8,154 @@ ATrackSpline::ATrackSpline() //: Super()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
 	Spline = CreateDefaultSubobject<USplineComponent>(FName("testSpline"));
 	TrackMesh = CreateDefaultSubobject<UStaticMesh>(FName("mesh"));
 	//TrackMesh = NewObject<UStaticMesh>(this,FName("mesh"));
 	//new(TrackMesh->SourceModels) FStaticMeshSourceModel();
 	AddedSplineMeshComponents.Empty();
-	
+
+	//ScoreTimeline = CreateDefaultSubobject<UTimelineComponent>(FName("TimelineScore"));
+
+	//InterpFunction.BindUFunction(this, FName{ TEXT("TimelineFloatReturn") });
+
 }
 
+
+
+void ATrackSpline::SetupCurve()
+{
+	auto richCurve = new FRichCurve();
+	auto key = richCurve->AddKey(0.0f, 0.0f); 
+	richCurve->AddKey(200.0f, 5.0f);
+	//richCurve->SetKeyTime(key, 10.0f);
+	richCurve->SetKeyInterpMode(key, RCIM_Linear);
+	
+	fCurve = NewObject<UCurveFloat>();
+	
+	auto list = fCurve->GetCurves();
+	list.Add(FRichCurveEditInfo(richCurve, FName{ TEXT("MyFirstCurve") }));
+	
+
+}
 // Called when the game starts or when spawned
 void ATrackSpline::BeginPlay()
 {
-	
 	Super::BeginPlay();
+
+	//ScoreTimeline->AddInterpFloat(fCurve, InterpFunction, FName{ TEXT("Floaty") });
+
+	 // or PlayFromStart() etc, can be called anywhere in this class
+
+	UWorld* world = GetWorld();
+	FActorSpawnParameters spawnParams;
+	spawnParams.Owner = this;
+
+	//blueprint.Get();
+	FVector startPoint = Spline->GetLocationAtSplinePoint(0, ESplineCoordinateSpace::World);
+	FRotator startRotation = Spline->GetRotationAtSplinePoint(0, ESplineCoordinateSpace::World);
+	actor = world->SpawnActor<ATrain>(blueprint, startPoint, startRotation, spawnParams);
+
+	//SetupCurve();
+	//static ConstructorHelpers::FObjectFinder<UCurveFloat> Curve(TEXT("/TestCurve"));
+	//check(Curve.Succeeded());
+
+	//fCurve = Curve.Object;
+
+	if (fCurve)
+	{
+
+		FOnTimelineFloat onTimelineCallback;
+
+		onTimelineCallback.BindUFunction(this, FName("TimelineCallback"));
+
+
+		myTimeLine.AddInterpFloat(fCurve, onTimelineCallback, FName("Alpha"));
+
+
+		myTimeLine.SetLooping(false);
+		startPoint = Spline->GetLocationAtSplinePoint(1, ESplineCoordinateSpace::World);
+		startRotation = Spline->GetRotationAtSplinePoint(1, ESplineCoordinateSpace::World);
+		auto makeTransform = FTransform(startRotation, startPoint, FVector(1.0f, 1.0f, 1.0f));
+		actor->SetActorTransform(makeTransform);
+
+		myTimeLine.PlayFromStart();
+	}
 	
+	
+	
+}
+
+
+void ATrackSpline::TimelineFinishedCallback()
+{
+	// This function is called when the timeline finishes playing.
+	UE_LOG(LogTemp, Warning, TEXT("time line has finished...."));
 }
 
 // Called every frame
 void ATrackSpline::Tick(float DeltaTime)
 {
+
 	Super::Tick(DeltaTime);
+	//UE_LOG(LogTemp, Warning, TEXT("DeltaTime %f"), DeltaTime);
+	myTimeLine.TickTimeline(DeltaTime);
+
 	
+	/*
+	if (ScoreTimeline != NULL) 
+	{ ScoreTimeline->TickComponent(DeltaTime, ELevelTick::LEVELTICK_TimeOnly, NULL); }
+	*/
 }
+
+void ATrackSpline::TimelineCallback(float val)
+{
+
+
+	UE_LOG(LogTemp, Warning, TEXT("interpolatedVal: %f"), val);
+	UE_LOG(LogTemp, Warning, TEXT("GetSplineLength: %f"), Spline->GetSplineLength());
+
+	//This function is called for every tick in the timeline.
+	float distance = Spline->GetSplineLength() * val;
+
+	auto makeTransform = FTransform(Spline->GetRotationAtDistanceAlongSpline(distance, ESplineCoordinateSpace::World),
+		Spline->GetLocationAtDistanceAlongSpline(distance, ESplineCoordinateSpace::World),
+		FVector(1.0f, 1.0f, 1.0f));
+	//ScoreTimeline->GetPlaybackPosition();
+
+	//targets location = FMath::Lerp(actors location , splinelocation, interpolatedVal);
+
+	//rotation, location, scale
+	//auto makeTransform = FTransform(FMath::Lerp(actor->GetActorRotation(), Spline->GetRotationAtDistanceAlongSpline(distance, ESplineCoordinateSpace::World), interpolatedVal),
+	//FMath::Lerp(actor->GetActorLocation(), Spline->GetLocationAtDistanceAlongSpline(distance, ESplineCoordinateSpace::World), interpolatedVal),
+	//FVector(1.0f, 1.0f, 1.0f));
+	//trainactor object SetWorldTransform(makeTransform)
+	actor->SetActorTransform(makeTransform);//SetActorRelativeTransform(makeTransform);
+
+	UE_LOG(LogTemp, Warning, TEXT("time line is running....  "));
+	//	UE_LOG(LogTemp, Warning, TEXT("play rate: %f"  ), ScoreTimeline->GetPlayRate());
+	//UE_LOG(LogTemp, Warning, TEXT("playbackpos: %f"  ), ScoreTimeline->GetPlaybackPosition());
+
+
+	/*
+	int i = 0;
+	FVector location, actorLocation;
+	FRotator rotation, actorRoation;
+	for (i; i < Spline->GetSplineLength(); i++)
+	{
+	location = Spline->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::World);
+	rotation = Spline->GetRotationAtSplinePoint(i, ESplineCoordinateSpace::World);
+
+	actorLocation = actor->GetActorLocation();
+	actorRoation = actor->GetActorRotation();
+
+	actor->SetActorLocation(FMath::Lerp(location, actorLocation, interpolatedVal));
+	actor->SetActorRotation(FMath::Lerp(rotation, actorRoation, interpolatedVal));
+
+	}
+	*/
+}
+
 
 void ATrackSpline::SetupSpline()
 {
@@ -57,7 +183,7 @@ void ATrackSpline::SetupSpline()
 	{
 		AddedSplineMeshComponents[Index]->CreationMethod = EComponentCreationMethod::UserConstructionScript;
 		AddedSplineMeshComponents[Index]->SetMobility(EComponentMobility::Movable);
-		AddedSplineMeshComponents[Index]->AttachTo(Spline);
+		AddedSplineMeshComponents[Index]->AttachToComponent(Spline,FAttachmentTransformRules::SnapToTargetIncludingScale);
 			//AttachParent = Spline;
 
 		AddedSplineMeshComponents[Index]->bCastDynamicShadow = false;
@@ -71,6 +197,59 @@ void ATrackSpline::SetupSpline()
 		AddedSplineMeshComponents[Index]->SetStartAndEnd(pointLocationStart, pointTangentStart, pointLocationEnd, pointTangentEnd);
 	}
 	RegisterAllComponents();
+
+}
+
+void ATrackSpline::moveTrain()
+{
+	int i = 0;
+	FVector location, actorLocation;
+	FRotator rotation, actorRoation;
+	for (i; i < Spline->GetSplineLength(); i++)
+	{
+		location = Spline->GetLocationAtSplinePoint(i, ESplineCoordinateSpace::World);
+		rotation = Spline->GetRotationAtSplinePoint(i, ESplineCoordinateSpace::World);
+
+		actorLocation = actor->GetActorLocation();
+		actorRoation = actor->GetActorRotation();
+
+		actor->SetActorLocation(FMath::Lerp(location, actorLocation, 0.5));
+		actor->SetActorRotation(FMath::Lerp(rotation, actorRoation, 0.5));
+
+	}
+	//targets location = FMath::Lerp(actors location , ?, ScoreTimeline.GetPlaybackPosition());
+
+}
+void ATrackSpline::someMethod()
+{
+	if (fCurve != NULL)
+	{
+		FOnTimelineFloat onTimelineCallback;
+
+		ScoreTimeline = NewObject<UTimelineComponent>(this, FName("TimelineAnimation"));
+		ScoreTimeline->CreationMethod = EComponentCreationMethod::UserConstructionScript; // Indicate it comes from a blueprint so it gets cleared when we rerun construction scripts 
+		this->BlueprintCreatedComponents.Add(ScoreTimeline); // Add to array so it gets saved 
+		ScoreTimeline->SetNetAddressable();	// This component has a stable name that can be referenced for replication
+
+		ScoreTimeline->SetPropertySetObject(this); // Set which object the timeline should drive properties on 
+		ScoreTimeline->SetDirectionPropertyName(FName("TimelineDirection"));
+
+		ScoreTimeline->SetLooping(false);
+		ScoreTimeline->SetTimelineLength(5.0f);
+		ScoreTimeline->SetTimelineLengthMode(ETimelineLengthMode::TL_LastKeyFrame);
+		ScoreTimeline->SetPlaybackPosition(0.0f, false); //LogTimeline: SetPlaybackPosition: No float property 'None' in 'BP_Track_8'
+														 //ScoreTimeline->SetPlayRate(0.5f);
+														 //Add the float curve to the timeline and connect it to your timelines's interpolation function 
+		onTimelineCallback.BindUFunction(this, FName{ TEXT("TimelineCallback") });
+		onTimelineFinishedCallback.BindUFunction(this, FName{ TEXT("TimelineFinishedCallback") });
+		ScoreTimeline->AddInterpFloat(fCurve, onTimelineCallback, FName("Alpha"));
+		ScoreTimeline->SetTimelineFinishedFunc(onTimelineFinishedCallback);
+
+		ScoreTimeline->RegisterComponent();
+	}
+
+	ScoreTimeline->PlayFromStart();
+
 }
 
 
--- Source/HVCCC2/TrackSpline.h
+++ Source/HVCCC2/TrackSpline.h
@@ -6,7 +6,17 @@
 #include "GameFramework/Actor.h"
 #include "Classes/Components/SplineComponent.h"
 #include "Classes/Components/SplineMeshComponent.h"
+#include "Runtime/Engine/Classes/Components/TimelineComponent.h"
+
+#include "GameFramework/Actor.h"
+#include "Train.h"
+#include "Runtime/CoreUObject/Public/UObject/ConstructorHelpers.h"
+#include "Runtime/Engine/Classes/Engine/World.h"
+
 #include "Engine/StaticMesh.h"
+
+
+
 #include "TrackSpline.generated.h"
 
 UCLASS()
@@ -21,7 +31,12 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
+
+	UFUNCTION()
+		void TimelineFinishedCallback();
+
+	UFUNCTION()
+		void TimelineCallback(float val);
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -29,12 +44,47 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Track")
 		void SetupSpline();
 
+	UFUNCTION(BlueprintCallable, Category = "Track")
+		void someMethod();
+
+
+	UFUNCTION()
+		void moveTrain();
+	
+	UFUNCTION()
+	void SetupCurve();
+
 	UPROPERTY(EditDefaultsOnly, Category = "TrackMeshVar")
 		 class UStaticMesh* TrackMesh;
 	
 	UPROPERTY(EditAnywhere, Category = "Track") //change to readOnly? dont really need it modified in bp?
 		class USplineComponent* Spline;
+
+	UPROPERTY(EditAnywhere, Category = "Train")
+	TSubclassOf<class ATrain> blueprint;
 	
- TArray< class USplineMeshComponent*> AddedSplineMeshComponents;
+	UPROPERTY(VisibleAnywhere, Category = "Track")
+		UTimelineComponent* ScoreTimeline;
+
+	UPROPERTY(EditAnywhere, Category = "Track")
+		UCurveFloat* fCurve;
+
+	UPROPERTY(VisibleAnywhere, Category = "Train")
+		ATrain *actor;
+
+	UPROPERTY(EditAnywhere)
+		TEnumAsByte<ETimelineDirection::Type> TimelineDirection;
+
+	//UPROPERTY(VisibleAnyWhere)
+		//float None;
 	
+	FTimeline myTimeLine;
+
+		
+	
+		FOnTimelineEventStatic onTimelineFinishedCallback {};
+
+	TArray< class USplineMeshComponent*> AddedSplineMeshComponents;
+
+
 };
