0f796f1288438fd8c02e806a7b9692a9237a10be
Legend9119 <nicholaskhendry@gmail.com>
2018-10-06 17:06:56 +1000

    Successfully got the script working with command line parameters.

--- Source/HVCCC2/data/python/remove_all_events_not_belonging_to_one_entity.py
+++ Source/HVCCC2/data/python/remove_all_events_not_belonging_to_one_entity.py
@@ -1,14 +1,31 @@
-xmlFilesDirectory = '../../../../XMLs/'
-xmlFileName = 'CCTDumpLog.xml'
-rootElement = 'events'
-idName = 'dumpstationID'
-entityID = 'CCT.RR1'
-outputFilesDirectory = '../../../../XMLs/'
+import argparse
 
-with open(xmlFilesDirectory + xmlFileName) as fd:
+parser = argparse.ArgumentParser(description='A simple script to strip all of the events not belonging to the entity specified, from the XML file selected.')
+parser.add_argument('-s', '--source-directory', type=str, required=True)
+parser.add_argument('-f', '--file-name', type=str, required=True)
+parser.add_argument('-r', '--root-element', type=str, required=True)
+parser.add_argument('-i', '--id-name', type=str, required=True)
+parser.add_argument('-e', '--entity-id', type=str, required=True)
+parser.add_argument('-o', '--output-directory', type=str)
+
+# Some example parameters:
+#     -s ../../../../XMLs/
+#     -f CCTDumpLog.xml
+#     -r events
+#     -i dumpstationID
+#     -e CCT.RR1
+
+args = parser.parse_args()
+
+outputFilesDirectory = args.source_directory
+
+if args.output_directory:
+    outputFilesDirectory = args.output_directory
+
+with open(args.source_directory + args.file_name) as fd:
     firstLine = True
 
-    newFileName = xmlFileName[:len(xmlFileName) - 4] + "_Only" + entityID + "Events.xml"
+    newFileName = args.file_name[:len(args.file_name) - 4] + "_Only" + args.entity_id + "Events.xml"
 
     for line in fd:
         modeUsed = 'a'
@@ -18,5 +35,5 @@ with open(xmlFilesDirectory + xmlFileName) as fd:
             firstLine = False
 
         with open(outputFilesDirectory + newFileName, modeUsed) as newFile:
-            if line == ("<" + rootElement + ">\n") or line == ("</" + rootElement + ">") or (idName + "=\"" + entityID + "\"") in line:
+            if line == ("<" + args.root_element + ">\n") or line == ("</" + args.root_element + ">") or (args.id_name + "=\"" + args.entity_id + "\"") in line:
                 newFile.write(line)
