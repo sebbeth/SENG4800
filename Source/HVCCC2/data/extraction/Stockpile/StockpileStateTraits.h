31b104e34540322e9003e205fec6c2c1a4ab5c10
MarcosCosmos <m.cosmos42@gmail.com>
2018-10-09 19:13:49 +1100

    Changed the Python script which extracted only one entity's events into its own XML file, to now do it for all unique entities in any file selected.

--- Source/HVCCC2/data/extraction/Stockpile/StockpileStateTraits.h
+++ Source/HVCCC2/data/extraction/Stockpile/StockpileStateTraits.h
@@ -8,16 +8,16 @@ template<>
 class StateTraits<StockpileState> {
 public:
     static StockpileState initializeFromEvent(const StockpileEvent& src) {
-        /* STUB: REPLACE WITH LOGIC FOR GUESSING THE INITIAL STATE FROM THE EVENT */
-        auto tentativeState = StockpileState::determineNextType(StockpileStateType::Idle, src.type);//see if the initial event is something that leaves the initial state; (addresses issue where some entities don't have their own creation event in the xml); still just a quickfix stub though
+		//see if the initial event is something that leaves the initial state; (addresses issue where some entities don't have their own creation event in the xml); still just a quickfix stub though:
+        auto tentativeState = StockpileState::determineNextType(StockpileStateType::Idle, src.type);
+
         return {src.id, tentativeState != StockpileStateType::Invalid ? tentativeState : StockpileStateType::Idle, src.hasAmount() ? src.amount : 0, src.length, src.machineID, src.padID, src.position, src.time, src.vesselID};
     }
 
     static StockpileState generateNextState(const StockpileState& current, const StockpileEvent& event) {
-
-        /* STUB: FILL IN WITH LOGIC FOR UPDATING ALL THE ATTRIBUTES */
 		StockpileState result = current;
 		result.type = StockpileState::determineNextType(current.type, event.type);
+
         if(event.hasAmount()) {
 			switch(event.type) {
 				case StockpileEventType::StackComplete:
@@ -33,28 +33,34 @@ public:
 		else {
 			result.amount = current.amount;
 		}
+
         if(event.hasLength()) {
-        result.length = event.length;
+			result.length = event.length;
 		}
 		else {
 			result.length = current.length;
 		}
-        if(event.hasMachineID()) {
-        result.machineID = event.machineID;
-        }
-        if(event.hasPadID()) {
-        result.padID = event.padID;
-        }
-        if(event.hasPosition()) {
-        result.position = event.position;
-        }
-        if(event.hasTime()) {
-        result.time = event.time;
-        }
-        if(event.hasVesselID()) {
-        result.vesselID = event.vesselID;
-        }
-        return result;
 
+        if(event.hasMachineID()) {
+			result.machineID = event.machineID;
+        }
+
+        if(event.hasPadID()) {
+			result.padID = event.padID;
+        }
+
+        if(event.hasPosition()) {
+			result.position = event.position;
+        }
+
+        if(event.hasTime()) {
+			result.time = event.time;
+        }
+
+        if(event.hasVesselID()) {
+			result.vesselID = event.vesselID;
+        }
+
+        return result;
     }
 };
